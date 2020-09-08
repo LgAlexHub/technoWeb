d059eba2b9d90fe7b84df51724f9e56cc0db7495
Alexis Legras <alexlegras@hotmail.com>
2020-09-08 22:12:36 +0200

    fin exo 1

--- prenom.php
+++ prenom.php
@@ -1,8 +1,31 @@
 <?php 	
-	$handle = fopen("dpt2018.csv", "r");
-	$data =[];
-	while(($row = fgetcsv($handle, 1000, ","))!==false){
-		array_push($data,$row);
-	}	
-	var_dump($data);
+
+function csvParser ($maxRow,$pathFile){
+	$handle = fopen($pathFile,"r");
+	$res = [];
+	$i = 0 ;
+	echo "<-- Start parsing --> \n";
+	while (($row = fgetcsv($handle,1000,","))!=FALSE && $i < $maxRow){
+		array_push($res, $row);
+		$i++;
+	}
+	return $res;
+}	
+
+function csvObjectExploder($csvTab){
+	$stringTab = ["sexe","prenom","annee","departement","nombre"];
+	for ($i = 0 ; $i < sizeof($csvTab) ;$i++){
+		$tmp = explode(";",$csvTab[$i][0]);
+		print_r($tmp);
+		$csvTab[$i][0] = [];
+		for ($j = 0 ; $j < sizeof($tmp) ; $j++){
+			$csvTab[$i][0] = array_merge($csvTab[$i][0],[$stringTab[$j]=>$tmp[$j]]);
+		}
+	}
+	return $csvTab;
+}
+	$csv_tab = csvParser(100,"dpt2018.csv");
+	$csv_tab = csvObjectExploder($csv_tab);
+	print_r($csv_tab);
+		
 ?>
