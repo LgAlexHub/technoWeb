0ee8f90c19697cf6e473817842b0e62b66ab5bc1
alex <alexlegras@hotmail.com>
2020-09-09 10:13:11 +0200

    correction mineur

--- prenom.php
+++ prenom.php
@@ -16,10 +16,9 @@ function csvObjectExploder($csvTab){
 	$stringTab = ["sexe","prenom","annee","departement","nombre"];
 	for ($i = 0 ; $i < sizeof($csvTab) ;$i++){
 		$tmp = explode(";",$csvTab[$i][0]);
-		print_r($tmp);
-		$csvTab[$i][0] = [];
+		$csvTab[$i]= [];
 		for ($j = 0 ; $j < sizeof($tmp) ; $j++){
-			$csvTab[$i][0] = array_merge($csvTab[$i][0],[$stringTab[$j]=>$tmp[$j]]);
+			$csvTab[$i] = array_merge($csvTab[$i],[$stringTab[$j]=>$tmp[$j]]);
 		}
 	}
 	return $csvTab;
